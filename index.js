97ffbb43d824b3bb63695c8e6b8f8a05d46a8f35
dongfengxin <dongfengxin@gmail.com>
2023-02-27 15:25:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
